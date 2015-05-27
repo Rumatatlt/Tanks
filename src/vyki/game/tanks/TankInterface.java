5f664cae2583f74058e5e6ad66e5189461b9fdb2
Rumatatlt <Rumatatlt@yandex.ru>
2015-05-27 17:01:27 +0400

    asd
    Signed-off-by: Rumatatlt <Rumatatlt@yandex.ru>

---
+++ src/vyki/game/tanks/TankInterface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vyki.game.tanks;
+
+/**
+ *
+ * @author selo0215
+ */
+public interface TankInterface {
+
+    public void create();
+}
