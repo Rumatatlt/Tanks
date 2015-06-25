beff291c43d85adcc338957812aceca631c12919
Rumata <Rumatatlt@yandex.ru>
2015-06-25 23:08:39 +0400

    Rumata

--- src/vyki/game/tanks/objects/Environment/AbstractTank.java
+++ src/vyki/game/tanks/objects/Environment/AbstractTank.java
@@ -2,6 +2,7 @@ package vyki.game.tanks.objects.Environment;
 
 import vyki.game.tanks.GlobalVariables;
 import vyki.game.tanks.Sprite;
+import vyki.game.tanks.objects.Shots.EnemyShot;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -23,6 +24,42 @@ public abstract class AbstractTank {
     public void Test(){
       System.out.println("Test");
     }
+
+/*    public void moveLeft(){
+        setX(getX() - speed);
+        getSprite().x= getX();
+        if (!"left".equals(getLastCourse())){
+            getSprite().setImage(getImage("tankLeft.png"));
+            setLastCourse("left");
+        }
+    }
+    public void moveRight(){
+        setX(getX() + speed);
+        getSprite().x= getX();
+        if (!"right".equals(getLastCourse())){
+            getSprite().setImage(getImage("tankRight.png"));
+            setLastCourse("right");
+        }
+    }
+    public void moveDown(){
+        setY(getY() + speed);
+        getSprite().y= getY();
+        if (!"down".equals(getLastCourse())){
+            getSprite().setImage(getImage("tankDown.png"));
+            setLastCourse("down");
+        }
+    }
+    public void moveUp(){
+        setY(getY() - speed);
+        getSprite().y= getY();
+        if (!"up".equals(getLastCourse())){
+            getSprite().setImage(getImage("tankUp.png"));
+            setLastCourse("up");
+        }
+    }
+    public void shoot(){
+        GlobalVariables.enemyShots.add(new EnemyShot(getLastCourse(), getX(), getY()));
+    }*/
 
     public Sprite getSprite() {
         return sprite;
@@ -49,5 +86,29 @@ public abstract class AbstractTank {
 
     public void setAlive(boolean alive) {
         this.alive = alive;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+
+    public String getLastCourse() {
+        return lastCourse;
+    }
+
+    public void setLastCourse(String lastCourse) {
+        this.lastCourse = lastCourse;
     }
 }
